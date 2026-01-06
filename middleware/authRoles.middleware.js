8a2eaaed5bff3b7bac014afaf2190d3905d43daf
Priyank Prajapati <priyankprajapati2609@gmail.com>
2026-01-06 10:34:49 +0530

    time

--- middleware/authRoles.middleware.js
+++ middleware/authRoles.middleware.js
@@ -1,10 +1,10 @@
 module.exports.authRole = (roles) => {
   return (req, res, next) => {
     if (!req.user) {
-      return res.status(403).json({ message: "Not Authorized" })
+      return res.status(401).json({ message: "Not Authorized" })
     }
     if (!roles.includes(req.user.role)) {
-      return res.status(401).json({ message: "You Don't Have Access" })
+      return res.status(403).json({ message: "You Don't Have Access" })
     }
     next()
   }
